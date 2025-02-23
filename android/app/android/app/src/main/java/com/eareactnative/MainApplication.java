c87bd21601138fb49586d6830a5dce518da7695a
Noorulain <ulnoor808@gmail.com>
2025-02-23 16:12:09 +0500

    Initial commit with all project files

---
+++ android/app/android/app/src/main/java/com/eareactnative/MainApplication.java
@@ -0,0 +1,47 @@
+package <your-package-here>;
+
+import android.app.Application;
++import com.facebook.react.PackageList;
++import com.facebook.react.ReactApplication;
++import com.facebook.react.ReactHost;
++import com.facebook.react.ReactNativeHost;
++import com.facebook.react.ReactPackage;
++import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
++import com.facebook.react.defaults.DefaultReactHost;
++import com.facebook.react.defaults.DefaultReactNativeHost;
++import com.facebook.soloader.SoLoader;
++import com.facebook.react.soloader.OpenSourceMergedSoMapping
++import java.util.List;
+
+-class MainApplication extends Application {
++class MainApplication extends Application implements ReactApplication {
++ @Override
++ public ReactNativeHost getReactNativeHost() {
++   return new DefaultReactNativeHost(this) {
++     @Override
++     protected List<ReactPackage> getPackages() { return new PackageList(this).getPackages(); }
++     @Override
++     protected String getJSMainModuleName() { return "index"; }
++     @Override
++     public boolean getUseDeveloperSupport() { return BuildConfig.DEBUG; }
++     @Override
++     protected boolean isNewArchEnabled() { return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED; }
++     @Override
++     protected Boolean isHermesEnabled() { return BuildConfig.IS_HERMES_ENABLED; }
++   };
++ }
+
++ @Override
++ public ReactHost getReactHost() {
++   return DefaultReactHost.getDefaultReactHost(getApplicationContext(), getReactNativeHost());
++ }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
++   SoLoader.init(this, OpenSourceMergedSoMapping);
++   if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
++     DefaultNewArchitectureEntryPoint.load();
++   }
+  }
+}
